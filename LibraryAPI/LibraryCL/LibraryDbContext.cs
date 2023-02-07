ff11b4b80e2069cd7db216fdbd77595f92cd61ce
Luka Miletic <dacamilena@gmail.com>
2023-02-07 17:19:52 +0100

    feat: configured entity framework, defined connection to local server and created first migration

---
+++ LibraryAPI/LibraryCL/LibraryDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryCL
+{
+    public class LibraryDbContext : DbContext
+    {
+        public LibraryDbContext(DbContextOptions<LibraryDbContext> options) : base(options)
+        {
+        }
+
+        protected LibraryDbContext()
+        {
+        }
+
+        private DbSet<Entity> _entities;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
