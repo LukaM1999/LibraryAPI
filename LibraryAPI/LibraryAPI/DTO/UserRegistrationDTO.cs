2d15b930ccb05719f63cd0a1b5df5ff01a57724a
Luka Miletic <dacamilena@gmail.com>
2023-02-09 16:13:42 +0100

    created user registration data transfer object

---
+++ LibraryAPI/LibraryAPI/DTO/UserRegistrationDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryAPI.DTO
+{
+    public class UserRegistrationDTO
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
