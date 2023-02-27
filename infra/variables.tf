b7bd57be013aaa722fe5c80c70f090d3fbdc3c2e
LukaM1999 <miletic.ra194.2018@uns.ac.rs>
2023-02-27 23:49:18 +0100

    Commit 3672208d: implemented codepipeline to create new docker image on every main branch push and update ecs

---
+++ infra/variables.tf
@@ -0,0 +1,18 @@
+variable "image_repo_name" {
+  default = "library_api"
+}
+variable "project_name" {
+  default = "LibraryAPI"
+}
+variable "image_tag" {
+  default = "latest"
+}
+variable "github_repo_owner" {
+  default = "LukaM1999"
+}
+variable "github_repo_name" {
+  default = "LibraryAPI"
+}
+variable "github_branch" {
+  default = "main"
+}
